1221879e86136f2ff35ad9a302ee347b021290c1
michael-hoo <hunan1217@icloud.com>
2017-12-22 16:33:14 +0800

    杂七杂八

--- hello.py
+++ hello.py
@@ -1,12 +1,14 @@
-from flask import Flask
+from flask import Flask, request, make_response, redirect
+from flask_script import Manager
 
 app = Flask(__name__)
+manager = Manager(app)
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return '<h1>Hello World!</h1>'
+    return redirect('https://www.baidu.com')
 
 
 @app.route('/user/<name>')
@@ -14,5 +16,26 @@ def user(name):
     return '<h1>Hello, {}!</h1>'.format(name)
 
 
+# response 返回状态码
+@app.route('/400')
+def bad_request():
+    return '<h1>Bad Request</h1>', 400
+
+
+# 从 request 中获取信息
+@app.route('/user-agent')
+def user_agent():
+    user_agent = request.headers.get('User-Agent')
+    return '<p>Your browser is %s</p>' % user_agent
+
+
+# 设置 cookie
+@app.route('/set-cookie')
+def set_cookie():
+    response = make_response('<h1>This document carries a cookie!</h1>')
+    response.set_cookie('answer', '42')
+    return response
+
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    manager.run()
