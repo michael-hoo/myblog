89cfa5e6967c2fd821f5ec71363e506fe4bfe22d
michael-hoo <hunan1217@icloud.com>
2017-12-26 16:42:51 +0800

    3. 实现"Web表单"

--- hello.py
+++ hello.py
@@ -1,42 +1,35 @@
-from flask import Flask, make_response, render_template
-from flask_script import Manager
+from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
-from datetime import datetime
+from flask_script import Manager
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
-# +3
+app.config['CSRF_ENABLED'] = True
+app.config['SECRET_KEY'] = 'hard to guess'
+
 manager = Manager(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
 
-@app.route('/')
-@app.route('/index')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    name = 'nathan hu'
-    dict = {'age': 25, 'gender': '男', 'salary': 8000}
-    return render_template('index.html', name=name, dict=dict,
-                           current_time=datetime.utcnow())
+    form = NameForm()
+    if form.validate_on_submit():
+        old_name = session.get('name')
+        if old_name is not None and old_name != form.name.data:
+            flash('Looks like you have changed your name.')
+        session['name'] = form.name.data
+        return redirect(url_for('index'))
+    return render_template('index.html', name=session.get('name'), form=form)
 
 
-# +1
-@app.route('/user/<name>')
-def user(name):
-    return 'Hello, %s' % name
-
-
-# +2
-@app.route('/cookie')
-def test_make_response():
-    response = make_response('<h1>This document carries a cookie!</h1>')
-    response.set_cookie('hunan', '25')
-    return response
-
-
-@app.errorhandler(404)
-def page_not_found(e):
-    return render_template('404.html'), 404
+class NameForm(FlaskForm):
+    name = StringField('What is your name?', validators=[DataRequired()])
+    submit = SubmitField('Submit')
 
 
 if __name__ == '__main__':
