659243c47ab3b2ea927f333fc8b5e00ce24a7e34
michael-hoo <hunan1217@icloud.com>
2017-12-26 14:49:04 +0800

    2.添加"模板"功能

--- hello.py
+++ hello.py
@@ -1,15 +1,23 @@
-from flask import Flask, make_response
+from flask import Flask, make_response, render_template
 from flask_script import Manager
+from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from datetime import datetime
 
 app = Flask(__name__)
 # +3
 manager = Manager(app)
+bootstrap = Bootstrap(app)
+moment = Moment(app)
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return 'Hello World!'
+    name = 'nathan hu'
+    dict = {'age': 25, 'gender': '男', 'salary': 8000}
+    return render_template('index.html', name=name, dict=dict,
+                           current_time=datetime.utcnow())
 
 
 # +1
@@ -24,6 +32,11 @@ def test_make_response():
     response = make_response('<h1>This document carries a cookie!</h1>')
     response.set_cookie('hunan', '25')
     return response
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
 
 
 if __name__ == '__main__':
